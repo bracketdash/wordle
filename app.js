6a9e6b0a665cbf6d60a77291bb5c44cf3acce3ec
Michael Hatch <michaelhatch@me.com>
2022-01-19 18:28:51 -0800

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function displayNextBestGuess() {
   const green = document.getElementById("green").value.toLowerCase();
   const yellow = [];
   const gray = document.getElementById("gray").value.toLowerCase();
-  
+
   const yellowStr = document.getElementById("yellow").value.toLowerCase();
   if (yellowStr.length) {
     yellowStr.split(";").forEach((seg) => {
@@ -10,8 +10,10 @@ function displayNextBestGuess() {
       yellow.push([ss[0], ss[1].split(",")]);
     });
   }
-  
-  const pattern = new RegExp(gray.length ? green.replace(/\./g, `[^${gray}]`) : green);
+
+  const pattern = new RegExp(
+    gray.length ? green.replace(/\./g, `[^${gray}]`) : green
+  );
   const filtered = words.filter((word) => {
     if (!pattern.test(word)) {
       return false;
@@ -39,10 +41,67 @@ function displayNextBestGuess() {
     }
     return true;
   });
-  // TODO: get statistical best next guess (for now just give the first word that qualifies)
-  document.getElementById('guess').innerHTML = filtered[0].toUpperCase();
+
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+  const freqs = {};
+  Array.from(Array(5).keys()).forEach((osition) => {
+    freqs[`p${osition}`] = alpha.reduce((o, l) => {
+      o[l] = 0;
+      return o;
+    }, {});
+  });
+  filtered.forEach((word) => {
+    Array.from(Array(5).keys()).forEach((osition) => {
+      freqs[`p${osition}`][word[osition]]++;
+    });
+  });
+  Array.from(Array(5).keys()).forEach((osition) => {
+    freqs[`p${osition}`] = Object.keys(freqs[`p${osition}`]).map((letter) => [
+      letter,
+      freqs[`p${osition}`][letter],
+    ]);
+    freqs[`p${osition}`].sort((a, b) =>
+      a[1] > b[1] ? 1 : a[1] < b[1] ? -1 : 0
+    );
+  });
+
+  const dot = ".";
+  let emptySlots = [];
+  let index;
+  let results = filtered;
+  let start = 0;
+  while ((index = green.indexOf(dot, start)) > -1) {
+    emptySlots.push(index);
+    start = index + 1;
+  }
+  emptySlots.reverse();
+  
+  const chosenLetters = [];
+  const getReducedSet = (wordset, osition) => {
+    if (wordset.length === 1) {
+      return wordset;
+    }
+    let letter = freqs[`p${osition}`].pop()[0];
+    while (freqs[`p${osition}`].length > 1 && chosenLetters.includes(letter)) {
+      letter = freqs[`p${osition}`].pop()[0];
+    }
+    const attempt = wordset.filter((w) => w[osition] === letter);
+    if (attempt.length) {
+      chosenLetters.push(letter);
+      return attempt;
+    } else {
+      return getReducedSet(wordset, osition);
+    }
+  };
+  emptySlots.forEach((osition) => {
+    results = getReducedSet(results, osition);
+  });
+
+  document.getElementById("guess").innerHTML = results[0].toUpperCase();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("button").addEventListener("click", displayNextBestGuess);
+  document
+    .getElementById("button")
+    .addEventListener("click", displayNextBestGuess);
 });
