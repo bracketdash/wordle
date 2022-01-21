4a2136836c4a137d78c831b0a0c49de6a1d24318
Michael Hatch <michaelhatch@me.com>
2022-01-21 08:37:43 -0800

    add plan for gamut script

--- gamut.js
+++ gamut.js
@@ -2,6 +2,11 @@
 // 1. Guess "SOARE"
 // 2. Get feedback
 // 3. Save guess with feedback to history array
-// 4. If feedback is not all green, make the next best guess and go back to step 2.
+// 4. If feedback is not all green, make the next best guess* and go back to step 2.
 // 5. If feedback is all green, save the history to the results array.
-// 6. Go to the next word in the word list then go back to step 1.
+
+// *next best guess: Look at the history array for the current word:
+//  GREENS - "....." with known letters (anywhere in history) filled in
+//  GRAYS - unique letters from full history
+//  NOTHERES - yellow letters and the positions we know they're not in
+//  SOMEWHERES - yellow letters that have not subsequently been marked green
