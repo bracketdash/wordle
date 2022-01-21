c6c457ebfc222a2603a8071f86d3047e1b4d6fe2
Michael Hatch <michaelhatch@me.com>
2022-01-21 08:30:34 -0800

    add plan for gamut script

---
+++ gamut.js
@@ -0,0 +1,7 @@
+// For each word in the word list:
+// 1. Guess "SOARE"
+// 2. Get feedback
+// 3. Save guess with feedback to history array
+// 4. If feedback is not all green, make the next best guess and go back to step 2.
+// 5. If feedback is all green, save the history to the results array.
+// 6. Go to the next word in the word list then go back to step 1.
